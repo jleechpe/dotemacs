8549dda12d1dad0bbab6508cbf9af2c1376bd71c
Jonathan Leech-Pepin <jonathan@leechpepin.com>
2023-08-09 14:50:00 -0400

    Enable Tempel for utilization

--- config/config-completion.el
+++ config/config-completion.el
@@ -144,7 +144,28 @@
 
 (use-package tempel
   :defer t
-  :elpaca nil)
+  :elpaca nil
+  :init
+  (defun tempel-setup-capf ()
+    ;; Add the Tempel Capf to `completion-at-point-functions'.
+    ;; `tempel-expand' only triggers on exact matches. Alternatively use
+    ;; `tempel-complete' if you want to see all matches, but then you
+    ;; should also configure `tempel-trigger-prefix', such that Tempel
+    ;; does not trigger too often when you don't expect it. NOTE: We add
+    ;; `tempel-expand' *before* the main programming mode Capf, such
+    ;; that it will be tried first.
+    (setq-local completion-at-point-functions
+                (cons #'tempel-expand
+                      completion-at-point-functions)))
+
+  ;; Optionally make the Tempel templates available to Abbrev,
+  ;; either locally or globally. `expand-abbrev' is bound to C-x '.
+  ;; (add-hook 'prog-mode-hook #'tempel-abbrev-mode)
+  (global-tempel-abbrev-mode)
+  :general
+  ("M-*" #'tempel-insert
+   "M-+" #'tempel-complete)
+  :hook ((conf-mode prog-mode text-mode) . tempel-setup-capf))
 
 ;; * Provide
 (provide 'config-completion)
